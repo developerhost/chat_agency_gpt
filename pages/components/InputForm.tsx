263f279bf8bdee479584e1a24d0dc9cb82cb718c
kojin <kanonhara0609@gmail.com>
2023-04-23 21:20:54 +0900

    message

--- pages/components/InputForm.tsx
+++ pages/components/InputForm.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useRef, useState, } from "react";
 import { Message } from "../../types/custom";
 
 type InputFormProps = {
@@ -9,6 +9,8 @@ const InputForm = ({ onSubmit }: InputFormProps) => {
   // input要素への参照を作成
   const inputMyRef = useRef<HTMLInputElement>(null);
   const inputYourRef = useRef<HTMLInputElement>(null);
+  const inputMessageRef = useRef<HTMLInputElement>(null);
+  const [inputVisible, setInputVisible] = useState(true);
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
@@ -16,6 +18,7 @@ const InputForm = ({ onSubmit }: InputFormProps) => {
     // input要素から直接値を取得
     const inputMyProfile = inputMyRef.current?.value;
     const inputYourProfile = inputYourRef.current?.value;
+    const inputMessage = inputMessageRef.current?.value;
 
     if (inputMyProfile && inputYourProfile) {
       // 親コンポーネントから提供されたonSubmit関数を介して送信されたメッセージを処理
@@ -25,6 +28,13 @@ const InputForm = ({ onSubmit }: InputFormProps) => {
       });
       inputMyRef.current.value = "";
       inputYourRef.current.value = "";
+      setInputVisible(false);
+    } else if(inputMessage){
+        onSubmit({
+            role: "user",
+            content: inputMessage,
+        });
+        inputMessageRef.current.value = "";
     }
   };
 
@@ -33,18 +43,30 @@ const InputForm = ({ onSubmit }: InputFormProps) => {
       onSubmit={handleSubmit}
       className="items-center p-4 border-t border-gray-200"
     >
+    {inputVisible &&
       <input
         type="text"
         ref={inputMyRef}
         className="w-full flex-grow px-4 py-2 border rounded-lg focus:outline-none focus:ring focus:border-blue-300"
         placeholder="自分のプロフィールをコピペ"
       />
+    }
+    {inputVisible &&
       <input
         type="text"
         ref={inputYourRef}
         className="w-full flex-grow px-4 py-2 border rounded-lg focus:outline-none focus:ring focus:border-blue-300"
         placeholder="相手のプロフィールをコピペ"
       />
+    }
+    {!inputVisible &&
+      <input
+        type="text"
+        ref={inputMessageRef}
+        className="w-full flex-grow px-4 py-2 border rounded-lg focus:outline-none focus:ring focus:border-blue-300"
+        placeholder="相手のメッセージをコピペ"
+      />
+    }
       <button
         type="submit"
         className="px-4 py-2 bg-blue-500 text-white rounded-lg focus:outline-none focus:ring focus:border-blue-300"
